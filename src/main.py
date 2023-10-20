a0f2548023e283ddd2911f1f1d64f2c303c00f4f
Ордин Сергей Игоревич <osi@maynitek.ru>
2023-10-20 20:54:04 +0500

    create func and test_func

--- src/main.py
+++ src/main.py
@@ -11,8 +11,29 @@ def data_masking(data, request_type: str):
         masked_last_part = last_part[:4] + ' ' + last_part[4:6] + '**' + ' ' + '****' + ' ' + last_part[-4:]
     else:
         masked_last_part = last_part[:4] + ' ' + '****' + ' ' + '****' + ' ' + last_part[-4:]
-    masked_s = ' '.join(parts[:-1] + [masked_last_part])
-    return masked_s
+    return ' '.join(parts[:-1] + [masked_last_part])
+
+
+def format_date(data):
+    """Форматирование даты"""
+    date_obj = dt.strptime(data['date'], "%Y-%m-%dT%H:%M:%S.%f")
+    return date_obj.strftime("%d.%m.%Y")
+
+
+def format_result(data, formatted_date):
+    """Форматирование результата"""
+    data_masked_to = data_masking(data['to'], "to")
+    if data.get('from'):
+        data_masked_from = data_masking(data['from'], "from")
+        result_data = (f"{formatted_date} {data['description']}\n"
+                       f"{data_masked_from} -> {data_masked_to}\n"
+                       f"{data['operationAmount']['amount']} {data['operationAmount']['currency']['name']}")
+    else:
+        result_data = (f"{formatted_date} {data['description']}\n"
+                       f"{data_masked_to}\n"
+                       f"{data['operationAmount']['amount']} {data['operationAmount']['currency']['name']}")
+
+    return result_data
 
 
 def main():
@@ -20,23 +41,12 @@ def main():
     datas = get_result_data()
     results = []
     for data in datas:
-        data_masked_to = data_masking(data['to'], "to")
-        date_obj = dt.strptime(data['date'], "%Y-%m-%dT%H:%M:%S.%f")
-        formatted_date = date_obj.strftime("%d.%m.%Y")
-        if 'from' in data:
-            data_masked_from = data_masking(data['from'], "from")
-            result = (f"{formatted_date} {data['description']}\n"
-                      f"{data_masked_from} -> {data_masked_to}\n"
-                      f"{data['operationAmount']['amount']} {data['operationAmount']['currency']['name']}")
-        else:
-            result = (f"{formatted_date} {data['description']}\n"
-                      f"{data_masked_to}\n"
-                      f"{data['operationAmount']['amount']} {data['operationAmount']['currency']['name']}")
-        results.append(result)
+        formatted_date = format_date(data)
+        result_data = format_result(data, formatted_date)
+        results.append(result_data)
     return results
 
 
 if __name__ == '__main__':
     for result in main():
         print(result + '\n')
-
